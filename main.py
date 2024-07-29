0794e3bd79aef26fc048f46206ec6986979f1b59
Arturo Suárez <manuel.suarez@cimat.mx>
2024-07-28 22:49:45 -0600

    Load data

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+DATA_DIR = './data/CamVid/'
+
+# load repo with data if it is not exists
+if not os.path.exists(DATA_DIR):
+    print('Loading data...')
+    os.system('git clone https://github.com/alexgkendall/SegNet-Tutorial ./data')
+    print('Done!')
+
